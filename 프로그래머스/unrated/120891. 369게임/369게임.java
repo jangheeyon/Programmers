918259beea948516d90735f7761b84287b5ff256
heeyon <138287325+jangheeyon@users.noreply.github.com>
2023-11-05 23:36:08 +0900

    [level unrated] Title: 369게임, Time: 0.03 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120891. 369게임/369게임.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+        String orderStr = String.valueOf(order);
+        for(int i=0; i<orderStr.length(); i++) {
+            if(orderStr.charAt(i) == '3'
+               || orderStr.charAt(i) == '6'
+               || orderStr.charAt(i) == '9') {
+               answer++;
+            }
+        }
+        return answer;
+    }
+}
