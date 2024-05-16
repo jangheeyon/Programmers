c839fb53fd12657f211a73d1a27d65b5c128d8c4
heeyon <138287325+jangheeyon@users.noreply.github.com>
2024-05-16 23:06:16 +0900

    [Silver IV] Title: 듣보잡, Time: 3880 ms, Memory: 46024 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+def solution(n, m, name):
+    answer = []
+    sH = defaultdict(int)
+    for x in name:
+        sH[x] += 1
+    for key in sH:
+        if sH[key] == 2:
+            answer.append(key)
+    print(len(answer))
+    for x in sorted(answer):
+        print(x)
+
+
+arr = []
+n, m = map(int, input().split())
+for i in range(n+m):
+    arr.append(input())
+solution(n, m, arr)
